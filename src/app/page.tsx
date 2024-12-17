52094783065398f2dcc0b3010b5e91d10a83d152
Kegen Guyll <guyllkegen@gmail.com>
2024-12-17 13:25:20 -0600

    enable dynamic import for Map component to support client-side rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
-import Map from "@/components/Map";
+'use client'
+
+import dynamic from 'next/dynamic'
+ 
+const Map = dynamic(
+  () => import('@/components/Map'),
+  { ssr: false }
+)
+ 
 
 export default function Home() {
   return (
