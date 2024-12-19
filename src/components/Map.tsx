2247a2c8b7b86af9f26b2ea027b6656307c3cdcc
Kegen Guyll <guyllkegen@gmail.com>
2024-12-19 12:46:25 -0600

    updated map to be more generic

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,60 +1,74 @@
 'use client'
 
-import React from 'react';
+import React, { useMemo } from 'react';
 import { MapContainer } from 'react-leaflet/MapContainer'
-import { TileLayer } from 'react-leaflet/TileLayer'
-import IcewindDaleLocationsLayer from './layers/icewindDale/locations';
-import type { LeafletMouseEvent } from 'leaflet';
-import { Settlement } from '@/types/settlement';
 import { useMapEvents } from 'react-leaflet';
-import MapContext from './MapContext';
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import type { LatLngExpression } from 'leaflet';
 
-type DeselectSettlementProps = {
-  setSelectedSettlement: React.Dispatch<React.SetStateAction<Settlement | undefined>>;
-}
 
-function DeselectSettlement({ setSelectedSettlement }: DeselectSettlementProps) {
+
+function SetUrlParams() {
+  const router = useRouter();
+  const pathname = usePathname();
+
   useMapEvents({
-    preclick() {
-      setSelectedSettlement(undefined);
-    },
+    moveend(e) {
+      const params = new URLSearchParams();
+      params.set("lat", e.target.getCenter().lat);
+      params.set("lng", e.target.getCenter().lng);
+      params.set("zoom", e.target.getZoom());
+      router.replace(`${pathname}?${params.toString()}`);
+    }
   });
 
   return null;
 }
 
-const Map: React.FC = () => {
-  const [selectedSettlement, setSelectedSettlement] = React.useState<Settlement | undefined>(undefined);
-  
-  const handleLocationClick = (e: LeafletMouseEvent, settlement?: Settlement) => {
-    setSelectedSettlement(settlement);
+  type MapProps = {
+    tileLayer:React.ReactNode
+    layerGroups?: React.ReactNode[]
+    mapOverlays?: React.ReactNode[]
+    zoomSettings?: {
+      min: number
+      max: number
+    }
   }
+
+const Map: React.FC<MapProps> = ({tileLayer, layerGroups, mapOverlays, zoomSettings}) => {
+  const searchParams = useSearchParams();
+  const zoom = searchParams.get('zoom');
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  const mapCenter: LatLngExpression = useMemo(() => {
+    if (lat && lng) {
+      return [parseFloat(lat), parseFloat(lng)];
+    }
+    return [52.71546403706492, 0.33897251478477286]
+  }, [lat, lng]);
 
   return (
     <div className='h-screen w-screen relative'>
-      <MapContainer 
+      <MapContainer   
         bounds={[
           [52.19990807412985, 1.5754550295695455],
           [53.23101999999999, -0.8975099999999998],
         ]}        
-        center={[52.71546403706492, 0.33897251478477286]} 
+        center={mapCenter} 
         className='w-full h-full'
         zoomControl={false}
+        minZoom={zoomSettings?.min}
+        maxZoom={zoomSettings?.max}
+        zoom={zoom ? +zoom : undefined}
+        dragging={true}
+        
       >
-        <TileLayer
-          minZoom={10}
-          maxZoom={13}
-          tms
-          url="/static/warped-icewind-dale-map/{z}/{x}/{y}.png"
-        />
-        <IcewindDaleLocationsLayer onLocationClick={handleLocationClick} />
-        <DeselectSettlement setSelectedSettlement={setSelectedSettlement} />
+        {tileLayer}
+        {layerGroups?.map((layerGroup) => layerGroup)}
+        <SetUrlParams />
       </MapContainer>
-      <MapContext
-        mapTitle='Icewind Dale'
-        selectedSettlement={selectedSettlement}
-        setSelectedSettlement={setSelectedSettlement}
-      />
+      {mapOverlays?.map((overlay) => overlay)}
     </div>
   );
 };
