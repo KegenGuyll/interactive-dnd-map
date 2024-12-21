f14d6c1a05896a31f016a2c9ecb9ab728053b575
Kegen Guyll <guyllkegen@gmail.com>
2024-12-20 20:53:04 -0600

    created a generic MapOverlay component to add consistency

--- src/components/BaseMapOverlay.tsx
+++ src/components/BaseMapOverlay.tsx
@@ -1,22 +1,36 @@
 import classNames from "classnames"
-import { HTMLAttributes } from "react"
+import React, { HTMLAttributes } from "react"
 
 
 type BaseMapOverlayProps = HTMLAttributes<HTMLDivElement> & {
   children: React.ReactNode
+  header?: React.ReactNode
+  childrenContainerStyle?: string
+  hideChildren?: boolean
 }
 
-const BaseMapOverlay: React.FC<BaseMapOverlayProps> = ({children, className, ...props}: BaseMapOverlayProps) => {
+const BaseMapOverlay: React.FC<BaseMapOverlayProps> = ({children, childrenContainerStyle, hideChildren = false, header, className, ...props}: BaseMapOverlayProps) => {
 
   return (  
     <div
       className={classNames(
-        'absolute shadow z-[9999] bg-white p-4 rounded text-black',
+        'absolute shadow z-[9999] bg-white rounded text-black overflow-y-auto',
         className
       )}
       {...props}
     >
-      {children}
+      <div>
+        {header && (
+          <div className="flex sticky top-0 p-2 md:pb-2 md:p-4 bg-white z-30">
+            {header}
+          </div>
+        )}
+        {!hideChildren && (
+          <div className={classNames('p-2 pt-0', childrenContainerStyle)}>
+            {children}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
